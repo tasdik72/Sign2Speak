c3dc7f3ae2d9e363251e4aa466c494746cc3570e
tasdik72 <tasdikabuyousuf@gmail.com>
2025-08-05 01:40:57 +0600

    fixed white screen

--- vite.config.ts
+++ vite.config.ts
@@ -24,9 +24,11 @@ export default defineConfig({
     },
   },
   root: path.resolve(import.meta.dirname, "client"),
+  base: "/",
   build: {
     outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
+    sourcemap: true,
   },
   server: {
     fs: {
